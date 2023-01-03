8fdf0ff0e3d8bfe7718073cd7020cbc5b99f2468
Nancy-GDA <34562364+Nancy-GDA@users.noreply.github.com>
2023-01-02 18:27:34 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "*** Installing Oh My zsh ***"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "*** creating symbolic links ***"
-ln $PWD/.zshrc ~/.szhrc
-ln $PWD/.gitconfig ~/.gitconfig
+ln -sf $PWD/.zshrc ~/.szhrc
+ln -sf $PWD/.gitconfig ~/.gitconfig
 
 gh auth login
